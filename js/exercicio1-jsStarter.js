5fb694329eadd11b50d5463d7525989cf7b65f4d
Renato Gonçalves <renatoslip@hotmail.com>
2020-01-23 10:01:45 -0300

    finalizado lista de ex 1

--- js/exercicio1-jsStarter.js
+++ js/exercicio1-jsStarter.js
@@ -1,15 +1,15 @@
 window.onload = function(){ 
-//exercicio 1
-var btn1 = document.getElementById('ex1');
-btn1.onclick = show;
+    //exercicio 1
+    var btn1 = document.getElementById('ex1');
+    btn1.onclick = show;
 
 
-var endereco = {
-    rua: "Rua dos pinheiros",
-    numero: 1293,
-    bairro: "Centro",
-    cidade: "São Paulo",
-        uf: "SP"
+    var endereco = {
+        rua: "Rua dos pinheiros",
+        numero: 1293,
+        bairro: "Centro",
+        cidade: "São Paulo",
+            uf: "SP"
     };
 
     function show(){
@@ -27,4 +27,54 @@ var endereco = {
         }
     }
     pares(32,321);
+
+    //exercicio 3
+    function temHabilidade(skills){
+        if (skills.indexOf('Javascript') != -1){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    var skills = ["Javascript", "ReactJS", "React Native"];
+    console.log(temHabilidade(skills));
+
+
+    //exercicio 4
+
+    function experiencia(anos) {
+        if (anos <= 1) {
+            return 'Iniciante';
+        }
+        if (anos <= 3) {
+            return 'Intermediário';
+        }
+        if (anos <= 6) {
+            return 'Avançado';
+        }
+        return 'Jedi Master';
+    }
+    var anosEstudo = 7;
+    console.log(experiencia(anosEstudo));
+    // De 0-1 ano: Iniciante
+    // De 1-3 anos: Intermediário
+    // De 3-6 anos: Avançado
+    // De 7 acima: Jedi Master
+
+    //exercicio 5
+    var usuarios = [
+        {
+            nome: "Diego",
+            habilidades: ["Javascript", "ReactJS", "Redux"]
+        },
+        {
+            nome: "Gabriel",
+            habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+        }
+    ];
+
+    for(var usuario of usuarios){
+        console.log(`O ${usuario.nome} possui as habilidades: `+ usuario.habilidades.join(', '));
+    }
 }
