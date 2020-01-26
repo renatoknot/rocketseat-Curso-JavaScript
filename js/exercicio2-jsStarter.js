bfce66b6696c8910eb508872ed446211f68f80aa
Renato Gonçalves <renatoslip@hotmail.com>
2020-01-26 09:54:39 -0300

    exercicio 2 do modulo 2 resolvido

--- js/exercicio2-jsStarter.js
+++ js/exercicio2-jsStarter.js
@@ -1,37 +1,25 @@
+    var btnElement = document.getElementById('btn');
+    var squaresElement = document.getElementById('squares');
 
-    //exercicio 1
-    function exibeQuadrado(){
-        var quadrado = document.createElement('div');
-
-        quadrado.setAttribute('id', 'quadrado')
-        quadrado.style.width = '100px';
-        quadrado.style.height = '100px';
-        quadrado.style.backgroundColor = 'red';
-
-        //posicao do quadrado aleatória
-        var p1 = Math.floor(Math.random() * 500);
-        var p2 = Math.floor(Math.random() * 400);
-        console.log(p1);
-        quadrado.style.left = p1+'px';
-        quadrado.style.top = p2+'px';
-        quadrado.style.position = 'absolute';
-        
-        //add o quadrado a tela
-        document.body.appendChild(quadrado);
-    }
     function getRandomColor() {
         var letters = "0123456789ABCDEF";
         var color = "#";
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
         return color;
     }
-    var newColor = getRandomColor();
-    console.log(newColor); 
-    addEventListener('mouseover', over)
 
-    function over(){
-        quadrado = document.getElementById('quadrado');
-        quadrado.style.background = newColor;
+    btnElement.onclick = function() {
+        var square = document.createElement('div');
+
+        square.style.width = '100px';
+        square.style.height = '100px';
+        square.style.backgroundColor = '#F00';
+        
+        square.onmouseover = function() {
+            square.style.backgroundColor = getRandomColor();
+        }
+
+        squaresElement.appendChild(square);
     }
